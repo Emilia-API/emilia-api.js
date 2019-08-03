6d913274b36323342b2b976d9944aaef5ad6247a
Sharif <43358217+SharifPoetra@users.noreply.github.com>
2019-08-03 18:24:01 +0800

    Please, we don't want use it like this

--- src/Classes/Client.js
+++ src/Classes/Client.js
@@ -1,19 +1,19 @@
-const ImageManipulation = require('./API/Image-Manipulation');
+const ManipulationImage = require('./API/ManipulationImage');
 
 /**
 * @class Client
 * @classdesc emilia-api.js - An API wrapper for https://emilia-api.glitch.me
 */
 class Client {
+
     /**
      * @author Fatir | Sakuta#2103
      * @param {String} token The token to pass the authentication api. 
      */
     constructor(token) {
 
-        this.author = "Fatir";
         this.version = require('../../package.json').version;
-        this.image = new ImageManipulation(token, 'https://emilia-api.glitch.me');
+        this.manipulationImage = new ManipulationImage(token, 'https://emilia-api.glitch.me');
         if (!token) return new Error("[EMILIA_API_ERROR] Missing parameter 'token'");
 
     }
