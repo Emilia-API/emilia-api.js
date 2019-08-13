425dd5246406b063058a840909b2146f60d3b197
Sharif <43358217+SharifPoetra@users.noreply.github.com>
2019-08-14 03:10:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Generators = require("./src/apis/Generators");
 const Effects = require("./src/apis/Effects");
-const Text = require("./src/apis/Text");
+const Generators = require("./src/apis/Generators");
+const Overlays = require("./src/apis/Overlays");
 const GIFs = require("./src/apis/GIFs");
+const Text = require("./src/apis/Text");
 const { version } = require("./package.json");
 
 /**
@@ -18,8 +19,9 @@ class EmiliaAPI {
 		if (typeof token !== "string") throw new Error("[EMILIA_API_ERROR] Please make sure your token is a string");
 		this.baseURL = "https://emilia-api.glitch.me/api";
 		this.version = version;
+                this.effects = new Effects(token, this.baseURL);
 		this.generators = new Generators(token, this.baseURL);
-		this.effects = new Effects(token, this.baseURL);
+                this.overlays = new Overlays(token, this.baseURL);
 		this.GIFs = new GIFs(token, this.baseURL);
 		this.text = new Text(token, this.baseURL);
 	}
