20129ffb30023d6c24678b1937494f71b182483f
Sharif <43358217+SharifPoetra@users.noreply.github.com>
2019-08-14 09:11:27 +0800

    Rewrite

--- src/apis/Text.js
+++ src/apis/Text.js
@@ -7,29 +7,43 @@ const { version } = require("../../package.json");
 class Text {
 	
 	constructor(token, baseURL) {
-		
-		/**
-		* A random word, most used for hangman
-		* @returns {Promise<object>}
-		*/
-		this.hangman = async () => {
-			const { body } = await get(`${baseURL}/hangman`)
-			.set("Authorization", `Bearer ${token}`)
-			.set("User-Agent", `Emilia-API Wrapper ${version}`);
-			return body;
-		}
-		
-		/**
-		* Get a random anime from https://randomanime.org
-		* @returns {Promise<object>}
-		*/
-		this.randomAnime = async () => {
-			const { body } = await get(`${baseURL}/random-anime`)
-			.set("Authorization", `Bearer ${token}`)
-			.set("User-Agent", `Emilia-API Wrapper ${version}`);
-			return body;
-		}
+		/** @access private */
+		this.token = token;
+		/** @access private */
+		this.baseURL = baseURL;
+	}
+	
+	/**
+	* A random word, most used for hangman
+	* @returns {Promise<hangmanResponse>}
+	*/
+	async hangman() {
+		const { body } = await get(`${this.baseURL}/hangman`)
+		.set("Authorization", `Bearer ${this.token}`)
+		.set("User-Agent", `Emilia-API Wrapper ${version}`);
+		return body;
+	}
+	
+	/**
+	* Get a random anime from https://randomanime.org
+	* @returns {Promise<randomAnimeResponse>}
+	*/
+	async randomAnime() {
+		const { body } = await get(`${this.baseURL}/random-anime`)
+		.set("Authorization", `Bearer ${this.token}`)
+		.set("User-Agent", `Emilia-API Wrapper ${version}`);
+		return body;
 	}
 }
 
 module.exports = Text;
+
+/**
+* @typedef {Object} hangmanResponse - The response for text.hangman
+* @prop {String} word - The random word
+*
+* @typedef {Object} randomAnimeResponse - The response for text.randomAnime
+* @prop {Array} details - The details of resulting anime
+* @prop {String} thumbnail - The thumbnail URL
+* @prop {String} description - The description of resulting anime
+*/
