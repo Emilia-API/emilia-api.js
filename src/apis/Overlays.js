207621b582a98a258c67a9669967f04b108bfef3
Sharif <43358217+SharifPoetra@users.noreply.github.com>
2019-08-14 09:00:18 +0800

    Rewrite and fix missing documentation

--- src/apis/Overlays.js
+++ src/apis/Overlays.js
@@ -7,32 +7,38 @@ const { version } = require("../../package.json");
 class Overlays {
 	
 	constructor(token, baseURL) {
-		
-		/**
-		* Draws a "approved" stamp over an image
-		* param {String} image - A URL to an image
-		*/
-		this.approved = async image => {
-			if (typeof image !== "string") throw new Error("[EMILIA-API-ERROR] Missing parameter image or that image isn't a string.");
-			const { body } = await get(`${baseURL}/approved`)
-			.set("Authorization", `Bearer ${token}`)
-			.set("User-Agent", `Emilia-API Wrapper ${version}`)
-			.query({ image });
-			return body;
-		}
-		
-		/**
-		* Draws a "rejected" stamp over an image
-		* param {String} image - A URL to an image
-		*/
-		this.rejected = async image => {
-			if (typeof image !== "string") throw new Error("[EMILIA-API-ERROR] Missing parameter image or that image isn't a string.");
-			const { body } = await get(`${baseURL}/rejected`)
-			.set("Authorization", `Bearer ${token}`)
-			.set("User-Agent", `Emilia-API Wrapper ${version}`)
-			.query({ image });
-			return body;
-		}
+		/** @access private */
+		this.token = token;
+		/** @access private */
+		this.baseURL = baseURL;
+	}
+	
+	/**
+	* Draws a "approved" stamp over an image
+	* @param {String} image - A URL to an image
+	* @returns {Promise<buffer>}
+	*/
+	async approved(image) {
+		if (typeof image !== "string") throw new Error("[EMILIA-API-ERROR] Missing parameter image or that image isn't a string.");
+		const { body } = await get(`${this.baseURL}/approved`)
+		.set("Authorization", `Bearer ${this.token}`)
+		.set("User-Agent", `Emilia-API Wrapper ${version}`)
+		.query({ image });
+		return body;
+	}
+	
+	/**
+	* Draws a "rejected" stamp over an image
+	* @param {String} image - A URL to an image
+	* @returns {Promise<buffer>} 
+	*/
+	async rejected(image) {
+		if (typeof image !== "string") throw new Error("[EMILIA-API-ERROR] Missing parameter image or that image isn't a string.");
+		const { body } = await get(`${this.baseURL}/rejected`)
+		.set("Authorization", `Bearer ${this.token}`)
+		.set("User-Agent", `Emilia-API Wrapper ${version}`)
+		.query({ image });
+		return body;
 	}
 }
 
